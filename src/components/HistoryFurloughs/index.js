a6cdc9d0bcf8f86451d04fc7f683e9363bd331f8
giang <vuthithaogiang050400@gmail.com>
2023-06-10 10:09:42 +0700

    add

--- src/components/HistoryFurloughs/index.js
+++ src/components/HistoryFurloughs/index.js
@@ -3,7 +3,10 @@ import styles from './HistoryFurloughs.module.scss';
 import { useEffect, useState } from 'react';
 import axios from '~/api/axios';
 import { useParams } from 'react-router';
-import { faL } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCalendar, faCheck, faClock, faFile, faFire, faXmark } from '@fortawesome/free-solid-svg-icons';
+import Tippy from '@tippyjs/react/headless';
+import { Wrapper as PopperWrapper } from '~/components/Popper';
 
 const cx = classNames.bind(styles);
 
@@ -41,11 +44,86 @@ function HistoryFurloughs() {
                         <h3>Recent Furloughs</h3>
                     </div>
 
-                    {data.map((item) => (
-                        <div className={cx('item')} key={item.furloughId}>
-                            {item.updateAt}
+                    <div className={cx('list-furlough')}>
+                        {/* 1 */}
+                        <div className={cx('item-sort')}>
+                            <span>Employee</span>
                         </div>
-                    ))}
+                        {/* 2 */}
+                        <div className={cx('item-sort')}>
+                            <FontAwesomeIcon icon={faClock} />
+                            <span>Update At</span>
+                        </div>
+                        {/* 3 */}
+                        <div className={cx('item-sort')}>
+                            <FontAwesomeIcon icon={faCalendar} />
+                            <span>Days off</span>
+                        </div>
+                        {/* 4 */}
+                        <div className={cx('item-sort')}>
+                            <FontAwesomeIcon icon={faFile} />
+                            <span>Note</span>
+                        </div>
+                        {/* 5 */}
+                        <div className={cx('item-sort')}>
+                            <FontAwesomeIcon icon={faFire} />
+                            <span>Status</span>
+                        </div>
+                        {data.map((item) => (
+                            <>
+                                {/* 1 */}
+                                <div className={cx('item')} key={item.furloughId}>
+                                    <span className={cx('avatar')}>
+                                        <img src={item.employee.avatar} alt="avatar" />
+                                    </span>
+                                </div>
+
+                                {/* 2 */}
+                                <div className={cx('item')}>
+                                    <span>{item.updateAt}</span>
+                                </div>
+
+                                <>
+                                    {/* 3 */}
+                                    <Tippy
+                                        interactive
+                                        render={(attrs) => (
+                                            <div className={cx('time-off')} tabIndex={'-1'} {...attrs}>
+                                                <PopperWrapper>
+                                                    <p className={cx('')}>
+                                                        From: <span className={cx('time-from')}>{item.offFrom}</span> -
+                                                        To: <span className={cx('time-to')}>{item.offTo}</span>
+                                                    </p>
+                                                </PopperWrapper>
+                                            </div>
+                                        )}
+                                        placement="right"
+                                    >
+                                        <div className={cx('item', 'days-off')}>
+                                            <span>📘 {item.totalDaysOff} days</span>
+                                        </div>
+                                    </Tippy>
+                                </>
+                                {/* 4 */}
+                                <div className={cx('item')}>
+                                    <span>{item.note}</span>
+                                </div>
+                                {/* 5 */}
+                                <div className={cx('item')}>
+                                    {item.status === 1 && (
+                                        <span className={cx('accept')}>
+                                            <FontAwesomeIcon icon={faCheck} />
+                                        </span>
+                                    )}
+                                    {item.status === 0 && (
+                                        <span className={cx('deny')}>
+                                            <FontAwesomeIcon icon={faXmark} />
+                                        </span>
+                                    )}
+                                </div>
+                            </>
+                        ))}
+                    </div>
                 </div>
             )}
         </>
